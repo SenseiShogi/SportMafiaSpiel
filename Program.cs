c93c8fc32ff9489e733a1cdde71f7f0f0ce6aea8
SenseiShogi <sensei.shogi@gmail.com>
2026-01-14 18:50:46 +0100

    Fix build: add explicit System using and stabilize PostgreSQL setup

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SportMafiaSpiel;
+
 
 namespace SportMafiaSpiel
 {
